985ded68e299a438efcc2d1db4d2bfd1b4ed9677
YerkoBarrientos <y.barrientos04@ufromail.cl>
2017-06-15 12:23:48 -0400

    Added "say" function

--- src/main/java/yerko/manejotexto/App.java
+++ src/main/java/yerko/manejotexto/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        say();
+    }
+    
+    public static void say(){
+    	System.out.println("Hello lady");
     }
 }
