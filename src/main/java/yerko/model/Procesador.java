2c731c952ab3fa805113f62ec0af93c34d442b52
YerkoBarrientos <y.barrientos04@ufromail.cl>
2017-06-16 12:56:19 -0400

    Agregado: función letraRepetida

--- src/main/java/yerko/model/Procesador.java
+++ src/main/java/yerko/model/Procesador.java
@@ -22,6 +22,7 @@ public class Procesador {
 	public Procesador(){
 		
 		leerArchivo();
+		letraRepetida('a');
 	}
 	
 	public void leerArchivo(){
@@ -49,5 +50,54 @@ public class Procesador {
 		}
 
 	}
+	
+	public void letraRepetida(char letra){
+		
+
+		long inicio = System.nanoTime();
+		
+		String linea;
+		int cuenta = 0;
+		
+		
+		try {
+			
+			lector = new FileReader(archivo);
+			Blector = new BufferedReader(lector);
+			
+			while((linea = Blector.readLine()) != null){
+				for (int i = 0; i < linea.length(); i++) {
+					if(linea.charAt(i)!=letra){
+						cuenta++;
+					}
+						
+				}
+			}
+
+			System.out.println("Veces repetida: "+cuenta);
+			long fin = System.nanoTime();
+			System.out.println("Tiempo para buscar letra: "+(fin-inicio)/100000+"ms");
+
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		finally{
+			
+			try {lector.close();}
+			
+			catch (IOException e) {
+				System.out.println("No se pudo abrir el archivo");
+				e.getMessage();
+				e.getStackTrace();
+				
+			}
+		}
+		
+	}
+	
+	
 
 }
